af407972468ac2215dc57774927a891f0064388d
wap8394 <157768094+wap8394@users.noreply.github.com>
2024-03-29 20:37:22 -0500

    Add files via upload

--- DNSServer.py
+++ DNSServer.py
@@ -110,7 +110,7 @@ dns_records = {
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address (what unique IP address is used here, similar to webserver lab) and port (the standard port for DNS)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Research this
-    server_socket.bind(('127.0.0.1', 1024))
+    server_socket.bind(("", 53))
 
     while True:
         try:
